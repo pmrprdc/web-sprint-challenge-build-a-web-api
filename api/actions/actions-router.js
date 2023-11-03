500e1ff95690cff60e52ddc1cba968757816fbaf
Pamir Perdeci <pamirperdeci@gmail.com>
2023-11-03 12:23:07 -0400

    actions router functional

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,15 @@
 // Write your "actions" router here!
+const express = require('express')
+const actionsRouter = express.Router();
+const Actions = require("./actions-model")
+
+actionsRouter.get('/', async(req,res)=>{
+    res.send("actions router functional")
+})
+
+
+
+
+
+
+module.exports = actionsRouter;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const server = express();
-
+const actionsRouter = require("./actions/actions-router")
 
 // Configure your server here
+server.use(express.json())
 // Build your actions router in /api/actions/actions-router.js
+server.use("/api/actions", actionsRouter)
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
 
