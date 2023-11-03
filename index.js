1a8a8763bf1803daa091ca80dfe9c1c6a181cfa6
Pamir Perdeci <pamirperdeci@gmail.com>
2023-11-03 12:17:50 -0400

    Made the port dynamic

--- index.js
+++ index.js
@@ -17,11 +17,12 @@ const server = require(
 "./api/server"
 )
 
+const port = 9000;
 
 
-server.listen(9000, ()=>{
+server.listen(port, ()=>{
 
 
-    console.log("Server running on specified port")
+    console.log(`Server running on port ${port}`)
     
 })
