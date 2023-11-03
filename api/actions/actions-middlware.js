ed4e8b2a00a1cca3badacfd3b1250e847011fe42
Pamir Perdeci <pamirperdeci@gmail.com>
2023-11-03 18:54:45 -0400

    created an empty middleware function that will check if the project exists at different endpoints

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,13 @@
 // add middlewares here related to actions
+
+const Actions = require('./actions-model')
+
+
+async function checkProjectExists(req,res,next) {
+
+}
+
+
+
+
+module.exports = { checkProjectExists}
