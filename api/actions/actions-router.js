58cf42f5fd5ca704d5008693a8d330fa4a597746
Pamir Perdeci <pamirperdeci@gmail.com>
2023-11-03 18:35:25 -0400

    actions router get request to api/actions returns actions

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,17 +1,14 @@
-// Write your "actions" router here!
-const express = require('express')
+const express = require('express');
 const actionsRouter = express.Router();
-const Actions = require("./actions-model")
+const Actions = require('./actions-model');
 
-actionsRouter.get('/', async(req,res)=>{
-    res.status(200).json({
-        message: "actions router able to return json"
-    })
-})
-
-
-
-
-
+actionsRouter.get('/', async(req, res) => {
+    const allActions = await Actions.get();
+  try {
+   res.status(201).json(allActions)
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred while fetching the actions.' });
+  }
+});
 
 module.exports = actionsRouter;
