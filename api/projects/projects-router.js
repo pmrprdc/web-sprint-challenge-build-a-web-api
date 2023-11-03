252bc9321fe60ef30ebbaafa505e78212e3fbc4a
Pamir Perdeci <pamirperdeci@gmail.com>
2023-11-03 18:47:41 -0400

    [GET] /api/projects         ✓ sends back all projects that exist.         ✓ sends back empty array if no projects.

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,15 @@
 // Write your "projects" router here!
+const express = require('express')
+const projectsRouter = express.Router();
+const Projects = require('./projects-model')
+
+
+
+projectsRouter.get('/', async(req,res)=>{
+
+    const allProjects = await Projects.get();
+    res.status(201).json(allProjects)
+})
+
+
+module.exports = projectsRouter;
--- api/server.js
+++ api/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const server = express();
 const actionsRouter = require("./actions/actions-router")
-
+const projectsRouter = require('./projects/projects-router')
 // Configure your server here
 server.use(express.json())
 // Build your actions router in /api/actions/actions-router.js
 server.use("/api/actions", actionsRouter)
+server.use("/api/projects", projectsRouter)
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
 
