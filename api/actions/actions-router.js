8f0fcd47b08c257dcfaa0dfffd0fd39e369f74fd
Pamir Perdeci <pamirperdeci@gmail.com>
2023-11-04 14:13:02 -0400

    all endpoints are meeting specs

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -38,9 +38,7 @@ actionsRouter.post('/', async (req, res) => {
         message: 'Please include notes, description, and project_id in the request body.',
       });
     }
-
     const newAction = await Actions.insert(req.body);
-
     res.status(201).json(newAction);
   } catch (err) {
     console.error('Error creating action:', err);
@@ -48,6 +46,50 @@ actionsRouter.post('/', async (req, res) => {
   }
 });
 
+
+actionsRouter.put('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { notes, description, completed, project_id } = req.body;
+
+    if (!notes || !description || !completed || !project_id) {
+      return res.status(400).json({
+        message: 'Please include notes, description, completed, and project_id in the request body.',
+      });
+    }
+
+    const action = await Actions.get(id);
+
+    if (!action) {
+      return res.status(404).json({ message: 'Action not found' });
+    }
+
+    const updatedAction = await Actions.update(id, req.body);
+
+    res.status(200).json(updatedAction);
+  } catch (err) {
+    console.error('Error updating action:', err);
+    res.status(500).json({ message: 'Something went wrong while trying to update the action.' });
+  }
+});
+
+actionsRouter.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const action = await Actions.get(id);
+
+    if (!action) {
+      return res.status(404).json({ message: 'Action not found' });
+    }
+
+    await Actions.remove(id);
+
+    res.status(204).send(); // 204 No Content for successful deletion
+  } catch (err) {
+    console.error('Error deleting action:', err);
+    res.status(500).json({ message: 'Something went wrong while trying to delete the action.' });
+  }
+});
 
 
 module.exports = actionsRouter;
