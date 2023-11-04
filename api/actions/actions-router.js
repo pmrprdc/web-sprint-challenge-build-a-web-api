61d84a3206a55e193cf14af59bb313df43a04cf1
Pamir Perdeci <pamirperdeci@gmail.com>
2023-11-04 14:10:51 -0400

    [GET] /api/actions/:id endpoint & [POST] /api/actions endpoint meet specs

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -13,6 +13,41 @@ actionsRouter.get('/', async(req, res) => {
 });
 
 
+actionsRouter.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const action = await Actions.get(id);
+    if (!action) {
+      return res.status(404).json({ message: 'Action not found' });
+    }
+
+    res.status(200).json(action);
+  } catch (err) {
+    console.error('Error getting action:', err);
+    res.status(500).json({ message: 'Something went wrong while trying to fetch the action.' });
+  }
+});
+
+
+actionsRouter.post('/', async (req, res) => {
+  try {
+    const { notes, description, project_id } = req.body;
+
+    if (!notes || !description || !project_id) {
+      return res.status(400).json({
+        message: 'Please include notes, description, and project_id in the request body.',
+      });
+    }
+
+    const newAction = await Actions.insert(req.body);
+
+    res.status(201).json(newAction);
+  } catch (err) {
+    console.error('Error creating action:', err);
+    res.status(500).json({ message: 'Something went wrong while trying to create the action.' });
+  }
+});
+
 
 
 module.exports = actionsRouter;
